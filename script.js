64fc4b6f4c79212067e6aacf845e83f399bec951
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-07-01 22:06:55 +0200

    adding third subsection to hero

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const heroUpperLeftPara = document.querySelector(".hero-section__upper-left p");
 const heroUpperLeftCube = document.querySelector(
   ".hero-section__upper-left-cube"
 );
+const heroMidRightPara = document.querySelector(".hero-section__mid-right p");
+const heroMidRightCube = document.querySelector(
+  ".hero-section__upper-right-cube"
+);
+
+console.log(heroMidRightCube);
+console.log(heroMidRightPara);
 
 // functions
 
@@ -14,5 +21,15 @@ heroUpperLeftPara.addEventListener("mouseover", () => {
 
 heroUpperLeftPara.addEventListener("mouseout", () => {
   heroUpperLeftCube.classList.remove("cube-hover");
+  console.log("mouse out");
+});
+
+heroMidRightPara.addEventListener("mouseover", () => {
+  heroMidRightCube.classList.add("cube-right-hover");
+  console.log("mouse in");
+});
+
+heroMidRightPara.addEventListener("mouseout", () => {
+  heroMidRightCube.classList.remove("cube-right-hover");
   console.log("mouse out");
 });
